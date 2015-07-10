03a1fa09ef0907871686384db579e305eaf36b4d
oPhuongXuongQuang <quangpx1@fsoft.com.vn>
2015-07-10 17:33:09 +0700

    #0 Create Repo

---
+++ ghinhap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ghinhap
+//
+//  Created by quangpx on 7/10/15.
+//  Copyright (c) 2015 fsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ghinhap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ghinhap
+//
+//  Created by quangpx on 7/10/15.
+//  Copyright (c) 2015 fsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
