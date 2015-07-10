2f2dd43e8bc60fbb7a3849f6eb7004f5f4a184c8
oPhuongXuongQuang <quangpx1@fsoft.com.vn>
2015-07-10 19:20:10 +0700

    #1 resize webview

--- ghinhap/ViewController.m
+++ ghinhap/ViewController.m
@@ -17,11 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *fullURL = @"http://ghinhap.com";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:requestObj];
+//    _webView.scalesPageToFit = YES;
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    
+    
+    _webView.frame=self.view.bounds;
+    
+    [_webView setUserInteractionEnabled: YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    // Disable user selection
+    [webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.style.webkitUserSelect='auto !important';"];
+    // Disable callout
+    [webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.style.webkitTouchCallout='auto !important';"];
 }
 
 @end
